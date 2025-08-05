df664ea7263dbf1cf4b114fe1ac06d80cb1fed4a
vakintosh <vakintosh@protonmail.com>
2025-08-05 17:44:56 -0600

    feat: Add dnssec resource

---
+++ internal/provider/resource_dnssec.go
@@ -0,0 +1,46 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type DnssecResourceModel struct {
+	ID      types.String `tfsdk:"id"`
+	Domain  types.String `tfsdk:"domain"`
+	Enabled types.Bool   `tfsdk:"enabled"`
+}
+
+func NewDnssecResource() resource.Resource {
+	return &DnssecResource{}
+}
+
+var _ resource.Resource = &DnssecResource{}
+
+type DnssecResource struct {
+	baseResource
+}
+
+func (r *DnssecResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_dnssec_record"
+}
+
+func (r *DnssecResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Manage a DNSSEC record for your domain",
+
+		Attributes: map[string]resourceSchema.Attribute{
+			"domain": resourceSchema.StringAttribute{
+				Required: true,
+			},
+			"enabled": resourceSchema.BoolAttribute{
+				MarkdownDescription: "Wheter or not to activate dnssec record",
+				Required:            true,
+			},
+		},
+	}
+}
